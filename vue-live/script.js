776f4e645e05e216abf9ab460f99316a3a558fd9
Július Retzer <jul.retzer@gmail.com>
2020-01-28 13:00:23 +0100

    Add kiwi to todo list

--- vue-live/script.js
+++ vue-live/script.js
@@ -2,7 +2,7 @@ var app = new Vue({
   el: "#app",
   data: {
     newTodo: "",
-    todos: ["Bananas", "Apples", "Avocados", "Oats"]
+    todos: ["Bananas", "Apples", "Avocados", "Oats", "Kiwi"]
   },
   methods: {
     addTodo() {
